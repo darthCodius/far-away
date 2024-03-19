48c58a2375524080e74183b2779cc6e1702fc66d
Abhinav Yadav <abhiyadavpky@gmail.com>
2024-03-19 17:36:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         items={items}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -111,10 +111,25 @@ function Item({ item, onDeleteItem, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list! 🧳</em>
+      </p>
+    );
+  const totalItems = items.length;
+  const numberOfPacked = items.filter((item) => item.packed);
+  const percentage = Math.round((numberOfPacked.length / totalItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list and you already packed X (X%)</em>
+      <em>
+        {percentage !== 100
+          ? `💼 You have ${totalItems} items on your list and you already packed
+        ${numberOfPacked.length} (${percentage}%)`
+          : `You got everything! Ready to go ✈`}
+      </em>
     </footer>
   );
 }
